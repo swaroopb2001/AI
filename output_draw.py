6681e6ccb7c3084b7f45057942f3d40999909c96
Swaroop Bharadwaj <83606630+swaroopb2001@users.noreply.github.com>
2022-11-29 09:56:34 +0530

    Create output_draw.py

---
+++ output_draw.py
@@ -0,0 +1,82 @@
+ | | 
+-+-+-
+ | | 
+-+-+-
+ | | 
+
+
+Computer goes first! Good luck.
+Positions are as follow:       
+1, 2, 3 
+4, 5, 6 
+7, 8, 9 
+
+
+X| | 
+-+-+-
+ | | 
+-+-+-
+ | | 
+
+
+Enter the position for 'O':  5
+X| | 
+-+-+-
+ |O| 
+-+-+-
+ | | 
+
+
+X|X| 
+-+-+-
+ |O| 
+-+-+-
+ | | 
+
+
+Enter the position for 'O':  3
+X|X|O
+-+-+-
+ |O| 
+-+-+-
+ | | 
+     
+     
+X|X|O
+-+-+-
+ |O| 
+-+-+-
+X| | 
+
+
+Enter the position for 'O':  4
+X|X|O
+-+-+-
+O|O|
+-+-+-
+X| |
+
+
+X|X|O
+-+-+-
+O|O|X
+-+-+-
+X| |
+
+
+Enter the position for 'O':  8
+X|X|O
+-+-+-
+O|O|X
+-+-+-
+X|O|
+
+
+X|X|O
+-+-+-
+O|O|X
+-+-+-
+X|O|X
+
+
+Draw!
